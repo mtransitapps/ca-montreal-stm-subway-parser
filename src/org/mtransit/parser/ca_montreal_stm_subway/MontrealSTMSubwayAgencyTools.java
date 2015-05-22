79768bfe48da841f47aeaaaca56c2e1e6e51aa7b
Mathieu Méa <mathieu.mea@gmail.com>
2015-05-22 08:59:26 -0400

    Compatibility with shared parser library & code cleaning.

--- src/org/mtransit/parser/ca_montreal_stm_subway/MontrealSTMSubwayAgencyTools.java
+++ src/org/mtransit/parser/ca_montreal_stm_subway/MontrealSTMSubwayAgencyTools.java
@@ -10,6 +10,7 @@ import org.mtransit.parser.Utils;
 import org.mtransit.parser.gtfs.data.GCalendar;
 import org.mtransit.parser.gtfs.data.GCalendarDate;
 import org.mtransit.parser.gtfs.data.GRoute;
+import org.mtransit.parser.gtfs.data.GSpec;
 import org.mtransit.parser.gtfs.data.GStop;
 import org.mtransit.parser.gtfs.data.GTrip;
 import org.mtransit.parser.mt.data.MAgency;
@@ -140,7 +141,7 @@ public class MontrealSTMSubwayAgencyTools extends DefaultAgencyTools {
 	private static final String SNOWDON = "Snowdon";
 
 	@Override
-	public void setTripHeadsign(MRoute mRoute, MTrip mTrip, GTrip gTrip) {
+	public void setTripHeadsign(MRoute mRoute, MTrip mTrip, GTrip gTrip, GSpec gtfs) {
 		String stationName = cleanStopName(gTrip.trip_headsign);
 		int directionId = -1;
 		if (stationName.contains(ANGRIGNON)) { // green
