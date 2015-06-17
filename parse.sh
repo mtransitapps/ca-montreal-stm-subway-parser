41aa850e89fe0e077964caa1b308af32819674a5
Mathieu Méa <mathieu.mea@gmail.com>
2015-06-17 12:04:39 -0400

    Compatibility with new parser.

--- parse.sh
+++ parse.sh
@@ -7,6 +7,7 @@ java -Xms2048m -Xmx8192m -Dfile.encoding=UTF-8 \
 bin:\
 $PARSER_DIRECTORY/bin:\
 $PARSER_DIRECTORY/lib/commons-csv-1.1.jar:\
+$PARSER_DIRECTORY/lib/commons-io-2.4.jar:\
 $PARSER_DIRECTORY/lib/commons-lang3-3.1.jar \
 $CLASS;
 echo ">> Parsing... DONE";
