956a3a8763521bada80d1850623fa64932cdbfd1
Mathieu Méa <mathieu.mea@gmail.com>
2017-07-08 16:35:02 -0400

    New list change

--- list_change.sh
+++ list_change.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
-echo ">> Listing change...";
-TARGET=$(cat "change_directory")
-git -C $TARGET status | grep "res/raw"
+source ../commons/commons.sh
+echo "> Listing change...";
+TARGET=$(cat "change_directory");
+RESULT=$(git -C $TARGET status);
+checkResult $? false;
+RESULT=$(echo $RESULT | grep "res/raw");
+STATUS=$?;
+if [ $STATUS == 1 ]; then STATUS=0; fi; # grep returns 1 when no result
+checkResult $STATUS false;
 git -C $TARGET diff res/values/gtfs_rts_values_gen.xml
-echo ">> Listing change... DONE";
+checkResult $?;
+RESULT=$(git -C $TARGET diff-index  --name-only HEAD -- "res/raw" | wc -l);
+if [ "$RESULT" -gt 0 ]; then
+	echo "> SCHEDULE CHANGED > MANUAL FIX!";
+	git -C $TARGET status | grep "res/raw" | head -n 7;
+	exit -1;
+fi
+echo "> Listing change... DONE";
