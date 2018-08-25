b17011d800959258d3893d9a8daaa45d9fb3fb81
Mathieu Méa <mathieu.mea@gmail.com>
2018-08-25 09:52:06 -0400

    Fix download script compatibility with URL containing spaces

--- download.sh
+++ download.sh
@@ -3,11 +3,11 @@ source ../commons/commons.sh
 echo ">> Downloading..."
 URL=`cat input_url`;
 mkdir -p input;
-download $URL "input/gtfs.zip";
+download "${URL}" "input/gtfs.zip";
 checkResult $?;
 if [ -e "input_url_next" ]; then
 	URL=`cat input_url_next`;
-	download $URL "input/gtfs_next.zip";
+	download "${URL}" "input/gtfs_next.zip";
 	checkResult $?;
 fi
 echo ">> Downloading... DONE"
